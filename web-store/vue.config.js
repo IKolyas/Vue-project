c36b71b088319ae6e4f81509398fb4c5e5a6da4a
IKolyas <i.kolyasnikov@mail.ru>
2020-09-08 22:20:23 +0500

    c

--- web-store/vue.config.js
+++ web-store/vue.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    devServer: {
-        port: 8080,
-        open: false,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                pathRewrite: {'^/api': '' },
-                secure: false,
-                changeOrigin: true
-            }
-        }
+    // devServer: {
+    //     port: 8080,
+    //     open: false,
+    //     proxy: {
+    //         '/api': {
+    //             target: 'http://localhost:3000',
+    //             pathRewrite: {'^/api': '' },
+    //             secure: false,
+    //             changeOrigin: true
+    //         }
+    //     }
 
-    },
+    // },
     publicPath: process.env.NODE_ENV === 'production'
     ? '/Vue-project/'
     : '/'
