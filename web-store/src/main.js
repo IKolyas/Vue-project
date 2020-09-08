ea8075b388fba7af07298e52e505bc12e4c31a12
IKolyas <i.kolyasnikov@mail.ru>
2020-09-09 00:46:49 +0500

    Update main.js

--- web-store/src/main.js
+++ web-store/src/main.js
@@ -1,18 +1,18 @@
 import 'bootstrap'
+import './layout/styles/styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/dropdown';
-import './layout/styles/styles.scss';
 import 'lodash'
 
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import app from './App.vue';
+import store from './store';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(app)
 }).$mount('#app')
