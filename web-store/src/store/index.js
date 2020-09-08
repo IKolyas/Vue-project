bf4ed17d0c402558c64e11550f2018bbf1180dca
IKolyas <i.kolyasnikov@mail.ru>
2020-09-09 01:54:38 +0500

    Update index.js

--- web-store/src/store/index.js
+++ web-store/src/store/index.js
@@ -12,8 +12,8 @@ export default new Vuex.Store({
     basketSum: null,
     // basketURL: "/api/basket",
     // catalogUrl: "/api/catalog",
-    basketURL: "https://github.com/IKolyas/Vue-project/tree/master/store_server/api/basket",
-    catalogUrl: "https://github.com/IKolyas/Vue-project/tree/master/store_server/api/catalog",
+    basketURL: "https://raw.githubusercontent.com/IKolyas/Vue-project/master/store_server/basket",
+    catalogUrl: "https://raw.githubusercontent.com/IKolyas/Vue-project/master/store_server/catalog",
     qItemPages: 6,
     priceFilter: {
       min: 0,
